cba85eb307587c835c7b5724b45aeb0dd6756483
racechao <racechao@gmail.com>
2026-01-15 20:50:04 +0800

    fix: build env

--- Frameworks/Storage/Package.swift
+++ Frameworks/Storage/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Storage",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macCatalyst(.v16),
     ],
     products: [
